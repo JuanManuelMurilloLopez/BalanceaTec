a1241c24ab214d0327e1d26fceeb9655a77b9a6b
JuanManuelMurilloLopez <142356988+JuanManuelMurilloLopez@users.noreply.github.com>
2024-11-18 12:48:29 -0600

    Add files via upload

---
+++ inicio_sesion.php
@@ -0,0 +1,39 @@
+<?php
+?>
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>BalanceaTec | Inicia Sesión</title>
+    <link rel="stylesheet" href="/Others/styles.css">
+</head>
+<body>
+    <div id="Titulo">
+        <h1>BalanceaTec</h1>
+    </div>
+    <div class="login">
+            <div class="login-screen">
+                <div class="app-title">
+                    <h1>Bienvenido</h1>
+                </div>
+    
+                <div class="login-form">
+                    <div class="control-group">
+                    <input type="text" class="login-field" value="" placeholder="usuario" id="login-name">
+                    <label class="login-field-icon fui-user" for="login-name"></label>
+                    </div>
+    
+                    <div class="control-group">
+                    <input type="password" class="login-field" value="" placeholder="contraseña" id="login-pass">
+                    <label class="login-field-icon fui-lock" for="login-pass"></label>
+                    </div>
+    
+                    <a class="btn btn-primary btn-large btn-block" href="#">Entrar</a>
+                    <a class="login-link" href="#">¿Olvidaste tu contraseña?</a>
+                </div>
+            </div>
+        </div>
+    
+</body>
+</html>
